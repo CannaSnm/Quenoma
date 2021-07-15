0d93a85f2f19e43bd672232d076a5936533a9ab3
CannaSnm <canavery.federico@gmail.com>
2021-07-15 11:05:41 -0300

    avance al 15/7/2021
    el JS ya funciona en casi todo su esplendor, me falta que vaya modificando el mensaje automaticamente.

--- carrito.js
+++ carrito.js
@@ -1,19 +1,35 @@
-var cantMargarita=0
-$(
-    "#p-margarita-cant"
-)
-.change(
-   (evento)=>{cantMargarita=evento.target.value
-     if(evento.target.value>0){
-    $("#p-margarita-check")
-    .prop('checked', true)
-       
-}
-else{$("#p-margarita-check")
-   .prop('checked', false)}
+var cantMargarita = 0
 
-   $("#mensaje").prop('innerText',"¡Hola Quenoma! queria pedir"+cantMargarita+"margarita")
-   $("#wasap").prop('href',"https://api.whatsapp.com/send?phone=5492216797230&text="+"¡Hola Quenoma! queria pedir"+cantMargarita+"margarita")
+/* este es para el mensajito
+        $("#mensaje").prop('innerText', "¡Hola Quenoma! queria pedir" + cantMargarita + "margarita")
+        $("#wasap").prop('href', "https://api.whatsapp.com/send?phone=5492216797230&text=" + "¡Hola Quenoma! queria pedir" + cantMargarita + "margarita")
+ */
 
-}
-)
+
+$(".sumar").change((evento)=>alCambiarNumero(evento)) // Buscamos Inputs tipo "number", cuando change ejecuta la funcion al cambiar//
+function alCambiarNumero (evento)
+{ //funcion al cambiarNumero//
+    var NombreProducto = evento.target.dataset.producto //guarda en una variable (var) que se llama "tipo" el producto que cambia//
+    var cantidad=evento.target.value //guarda en una Var, que se llama "cantidad" el valor//
+        if (cantidad> 0) {
+            $('*[data-check='+ NombreProducto+']') // busca ($) el input que tenga la propiedad ej: data-check='margarita'//
+                .prop('checked', true) //al input encontrado se le aplica el valor de esta propiedad//
+        }
+        else {
+            $('*[data-check='+ NombreProducto+']')
+            .prop('checked', false)
+        }
+
+} //hasta aca funcion al cambiar//
+$(".checkboxes").change((evento)=>Alcheckear(evento)) //buscamos ($) la clase .checkboxes, cuando change ejecuta la funcion Alcheckear//
+function Alcheckear (evento) //funcion Alcheckear recibe el evento"
+    { 
+        var NombreProducto= evento.target.dataset.check //variable NombreProducto guarda la info del data-check del input//
+        var EstaCheckeado=evento.target.checked //Variable EstaCheckeado guarda el estado del check-box// 
+            if (!EstaCheckeado) //si no esta chequeado continua//
+             {
+                $('*[data-producto='+ NombreProducto+']')//busca el input que tenga la prop. data-producto=NombreProducto ej: data-producto='margarita'//
+                .val(0)
+            }
+    }
+    
